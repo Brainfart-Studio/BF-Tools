921fb1a9ccdb70ffa1064ee6e41951be012d0c8c
Mitchell Opitz <mitchell@brainfart.studio>
2026-07-27 22:35:54 -0500

    feat - Add UnityConsoleSink implementation

--- Packages/Core/Logger/Runtime/Sinks/UnityConsoleSink.cs
+++ Packages/Core/Logger/Runtime/Sinks/UnityConsoleSink.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class UnityConsoleSink : MonoBehaviour
+namespace BFTools.Core.Logger
 {
-    // Start is called before the first frame update
-    void Start()
+    public class UnityConsoleSink : IBFLoggerSink
     {
-        
-    }
+        public void Write(LogLevel level, string[] tags, string message, Object context, bool includeStackTrace)
+        {
+            string formatted = Format(level, tags, message);
+            LogType logType = ToLogType(level);
+            LogOption logOption = includeStackTrace ? LogOption.None : LogOption.NoStacktrace;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            Debug.LogFormat(logType, logOption, context, formatted);
+        }
+
+        private static string Format(LogLevel level, string[] tags, string message)
+        {
+            string tagText = tags != null && tags.Length > 0 ? string.Join(",", tags) : string.Empty;
+            return $"<color={ColorFor(level)}>[{level}]</color> [{tagText}] {message}";
+        }
+
+        private static string ColorFor(LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.Trace:
+                case LogLevel.Debug:
+                    return "grey";
+                case LogLevel.Warning:
+                    return "yellow";
+                case LogLevel.Error:
+                case LogLevel.Critical:
+                    return "red";
+                default:
+                    return "white";
+            }
+        }
+
+        private static LogType ToLogType(LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.Warning:
+                    return LogType.Warning;
+                case LogLevel.Error:
+                case LogLevel.Critical:
+                    return LogType.Error;
+                default:
+                    return LogType.Log;
+            }
+        }
     }
 }
