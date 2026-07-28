8ea0b4e9c7683a8a4714b65e61cbccba774551fe
Mitchell Opitz <mitchell@brainfart.studio>
2026-07-28 10:33:26 -0500

    feat - Add BFLogger Trace calls to BFServiceLocator for register/unregister/miss

--- Packages/Core/ServiceLocator/Runtime/BFServiceLocator.cs
+++ Packages/Core/ServiceLocator/Runtime/BFServiceLocator.cs
@@ -1,25 +1,31 @@
 using System;
 using System.Collections.Generic;
+using BFTools.Core.Logger;
 
 namespace BFTools.Core.ServiceLocator
 {
     public static class BFServiceLocator
     {
+        private const string LogTag = "ServiceLocator";
+
         private static readonly Dictionary<Type, object> services = new Dictionary<Type, object>();
 
         public static void Register<T>(T service)
         {
             services[typeof(T)] = service;
+            BFLogger.Trace(LogTag, $"Registered {typeof(T).Name}");
         }
 
         public static T Get<T>()
         {
+            BFLogger.Trace(LogTag, $"Get {typeof(T).Name}");
             return (T)services[typeof(T)];
         }
 
         public static void Unregister<T>()
         {
             services.Remove(typeof(T));
+            BFLogger.Trace(LogTag, $"Unregistered {typeof(T).Name}");
         }
     }
 }
