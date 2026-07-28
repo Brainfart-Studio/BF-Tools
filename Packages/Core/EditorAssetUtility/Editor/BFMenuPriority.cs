0a36204ed2e43c79632979381fd341e1daa1c973
Mitchell Opitz <mitchell@brainfart.studio>
2026-07-28 15:27:30 -0500

    fix - Add missing namespace to BFMenuPriority

--- Packages/Core/EditorAssetUtility/Editor/BFMenuPriority.cs
+++ Packages/Core/EditorAssetUtility/Editor/BFMenuPriority.cs
@@ -1,21 +1,24 @@
-public static class BFMenuPriority
+namespace BFTools.Core.EditorAssetUtility.Editor
 {
-    public static class Group
+    public static class BFMenuPriority
     {
-        public const int Core = -100;
-        public const int Feedback = -80;
-    }
+        public static class Group
+        {
+            public const int Core = -100;
+            public const int Feedback = -80;
+        }
 
-    public static class Module
-    {
-        // Core
-        public const int GlobalBootstrapper = 1;
-        public const int LevelBootstrapper = 2;
-        public const int ObjectPooler = 3;
-        public const int Logger = 4;
+        public static class Module
+        {
+            // Core
+            public const int GlobalBootstrapper = 1;
+            public const int LevelBootstrapper = 2;
+            public const int ObjectPooler = 3;
+            public const int Logger = 4;
 
-        // Feedback
-        public const int Haptics = 1;
-        public const int ScreenShake = 2;
+            // Feedback
+            public const int Haptics = 1;
+            public const int ScreenShake = 2;
+        }
     }
 }
