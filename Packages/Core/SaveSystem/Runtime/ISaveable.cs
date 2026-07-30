4675d2e73b964fcf533de32934d0140581114db3
Mitchell Opitz <mitchell@brainfart.studio>
2026-07-30 11:36:40 -0500

    feat - Add StateType property to ISaveable contract

--- Packages/Core/SaveSystem/Runtime/ISaveable.cs
+++ Packages/Core/SaveSystem/Runtime/ISaveable.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace BFTools.Core.SaveSystem
 {
     public interface ISaveable
     {
+        Type StateType { get; }
         object CaptureState();
         void RestoreState(object state);
     }
