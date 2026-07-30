9e8883877f4dd16ab2a8345c45995b7d712b1945
Mitchell Opitz <mitchell@brainfart.studio>
2026-07-29 21:58:36 -0500

    feat - Add SaveSerializer wrapper (JSON conversion via Newtonsoft)

---
+++ Packages/Core/SaveSystem/Runtime/BFSaveSerializer.cs
@@ -0,0 +1,29 @@
+using Newtonsoft.Json;
+using Unity.Plastic.Newtonsoft.Json;
+
+namespace BFTools.Core.SaveSystem
+{
+    public static class BFSaveSerializer
+    {
+        private static readonly JsonSerializerSettings settings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.None,
+            TypeNameHandling = TypeNameHandling.Auto
+        };
+
+        public static string Serialize(object data)
+        {
+            return JsonConvert.SerializeObject(data, settings);
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json, settings);
+        }
+
+        public static object Deserialize(string json, System.Type type)
+        {
+            return JsonConvert.DeserializeObject(json, type, settings);
+        }
+    }
+}
