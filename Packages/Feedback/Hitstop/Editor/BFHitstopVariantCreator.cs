6ab72c6e593ea7ef00e9f0dfbb9a64cc543fb679
Mitchell Opitz <mitchell@brainfart.studio>
2026-07-30 15:55:44 -0500

    feat - Add BFHitstopVariantCreator prefab variant creation tool

---
+++ Packages/Feedback/Hitstop/Editor/BFHitstopVariantCreator.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+using BFTools.Core.EditorAssetUtility.Editor;
+namespace BFTools.Feedback.Hitstop.Editor
+{
+    public static class BFHitstopVariantCreator
+    {
+        private const string BasePrefabPath = "Packages/com.bftools.feedback/Hitstop/Prefabs/Hitstop.prefab";
+        private const string TargetPath = "Assets/Prefabs/Feedback";
+        private const string AssetName = "Hitstop.prefab";
+        [MenuItem("Assets/Create/BFTools/Feedback/Prefabs/Hitstop", priority = BFMenuPriority.Group.Feedback + BFMenuPriority.Module.Hitstop)]
+        private static void Create()
+        {
+            BFEditorAssetUtility.CreatePrefabVariant(BasePrefabPath, TargetPath, AssetName);
+        }
+    }
+}
