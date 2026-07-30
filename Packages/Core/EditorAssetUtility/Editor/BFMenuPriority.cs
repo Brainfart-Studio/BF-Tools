e72290171cd7d7e13960179f075beb5f76b05122
Mitchell Opitz <mitchell@brainfart.studio>
2026-07-30 15:48:14 -0500

    refactor - Rename BFHitstopConfigEditor to BFHitstopConfigCreator with menu priority ordering

--- Packages/Core/EditorAssetUtility/Editor/BFMenuPriority.cs
+++ Packages/Core/EditorAssetUtility/Editor/BFMenuPriority.cs
@@ -7,7 +7,6 @@ namespace BFTools.Core.EditorAssetUtility.Editor
             public const int Core = -100;
             public const int Feedback = -80;
         }
-
         public static class Module
         {
             // Core
@@ -15,10 +14,10 @@ namespace BFTools.Core.EditorAssetUtility.Editor
             public const int LevelBootstrapper = 2;
             public const int ObjectPooler = 3;
             public const int Logger = 4;
-
             // Feedback
             public const int Haptics = 1;
             public const int ScreenShake = 2;
+            public const int Hitstop = 3;
         }
     }
 }
