c43f5e4d30f26f7beb525fcbc2e221f302eca7a1
Mitchell Opitz <mitchell@brainfart.studio>
2026-07-31 13:12:13 -0500

    feat - Add BFAuroraRibbonsEffect implementation

---
+++ Packages/Visuals/Backgrounds/Runtime/Effects/AuroraRibbons/BFAuroraRibbonsEffect.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BFTools.Visuals.Background.AuroraRibbons
+{
+    public class BFAuroraRibbonsEffect : IBFBackgroundEffect
+    {
+        private readonly BFAuroraRibbonsConfig config;
+        private readonly List<BFAuroraRibbon> ribbons = new List<BFAuroraRibbon>();
+        private readonly List<BFBackgroundStar> stars = new List<BFBackgroundStar>();
+        private float elapsedTime;
+
+        public BFAuroraRibbonsEffect(BFAuroraRibbonsConfig config)
+        {
+            this.config = config;
+        }
+
+        public void Init()
+        {
+            elapsedTime = 0f;
+
+            ribbons.Clear();
+            for (int i = 0; i < config.RibbonCount; i++)
+                ribbons.Add(new BFAuroraRibbon(i, config));
+
+            stars.Clear();
+            for (int i = 0; i < config.StarCount; i++)
+                stars.Add(new BFBackgroundStar(config));
+        }
+
+        public void Tick(float dt)
+        {
+            elapsedTime += dt * config.WaveSpeed;
+
+            for (int i = 0; i < stars.Count; i++)
+                stars[i].Tick(dt);
+
+            for (int i = 0; i < ribbons.Count; i++)
+                ribbons[i].Tick(elapsedTime);
+        }
+
+        public void Cleanup()
+        {
+            ribbons.Clear();
+            stars.Clear();
+        }
+    }
+}
