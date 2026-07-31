d35362251cd5b802866b078ffd56936983c43553
Mitchell Opitz <mitchell@brainfart.studio>
2026-07-31 09:40:28 -0500

    feat - Warn on duplicate palette entry names

--- Packages/Visuals/Palette/Runtime/BFPaletteConfig.cs
+++ Packages/Visuals/Palette/Runtime/BFPaletteConfig.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using BFTools.Core.EventBus;
+using BFTools.Core.Logger;
 
 namespace BFTools.Visuals.Palette
 {
@@ -14,13 +15,30 @@ namespace BFTools.Visuals.Palette
 
     public class BFPaletteConfig : ScriptableObject
     {
+        private const string LogTag = "PaletteConfig";
+
         [SerializeField] private List<BFPaletteEntry> entries = new List<BFPaletteEntry>();
 
         public IReadOnlyList<BFPaletteEntry> Entries => entries;
 
         private void OnValidate()
         {
+            WarnOnDuplicateNames();
             EventBus<BFPaletteConfigChangedEvent>.Fire(new BFPaletteConfigChangedEvent { config = this });
+        }
+
+        private void WarnOnDuplicateNames()
+        {
+            var seen = new HashSet<string>();
+            for (int i = 0; i < entries.Count; i++)
+            {
+                if (!seen.Add(entries[i].name))
+                {
+                    BFLogger.Warning(LogTag,
+                        $"Duplicate entry name '{entries[i].name}' in '{name}'. First one wins.",
+                        this);
+                }
+            }
         }
     }
 }
