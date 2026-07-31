63f07878d51494454b9fd4cecf9cceff9440f9d6
Mitchell Opitz <mitchell@brainfart.studio>
2026-07-31 13:10:14 -0500

    feat - Add BFAuroraRibbonsConfig ScriptableObject

---
+++ Packages/Visuals/Backgrounds/Runtime/Effects/AuroraRibbons/BFAuroraRibbonsConfig.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BFTools.Visuals.Background.AuroraRibbons
+{
+    [CreateAssetMenu(fileName = "BFAuroraRibbonsConfig", menuName = "BFTools/Visuals/Background/Aurora Ribbons Config")]
+    public class BFAuroraRibbonsConfig : BFBackgroundConfig
+    {
+        [SerializeField] private Color baseTop = new Color(0.016f, 0.024f, 0.067f);
+        [SerializeField] private Color baseBottom = new Color(0.039f, 0.047f, 0.118f);
+        [SerializeField]
+        private List<Color> ribbonColors = new List<Color>
+        {
+            new Color(0.5f, 1f, 0.831f),
+            new Color(0.5f, 0.608f, 1f),
+            new Color(0.765f, 0.5f, 1f),
+            new Color(1f, 0.5f, 0.722f)
+        };
+        [SerializeField, Range(1, 8)] private int ribbonCount = 4;
+        [SerializeField, Range(0.05f, 1.5f)] private float waveSpeed = 0.35f;
+        [SerializeField, Range(20f, 260f)] private float amplitude = 110f;
+        [SerializeField, Range(1f, 10f)] private float thickness = 3f;
+        [SerializeField, Range(0, 60)] private float glow = 18f;
+        [SerializeField, Range(0, 300)] private int starCount = 120;
+        [SerializeField] private bool twinkle = true;
+
+        internal Color BaseTop => baseTop;
+        internal Color BaseBottom => baseBottom;
+        internal IReadOnlyList<Color> RibbonColors => ribbonColors;
+        internal int RibbonCount => ribbonCount;
+        internal float WaveSpeed => waveSpeed;
+        internal float Amplitude => amplitude;
+        internal float Thickness => thickness;
+        internal float Glow => glow;
+        internal int StarCount => starCount;
+        internal bool Twinkle => twinkle;
+
+        public override IBFBackgroundEffect CreateEffect()
+        {
+            return new BFAuroraRibbonsEffect(this);
+        }
+    }
+}
