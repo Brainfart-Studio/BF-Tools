4e0e76848313e267bde0d907dfdbed96136f60f2
Mitchell Opitz <mitchell@brainfart.studio>
2026-08-01 08:24:48 -0500

    feat - Add BFGradientLayerConfig ScriptableObject

---
+++ Packages/Visuals/Background/Runtime/Layers/Gradient/BFGradientLayerConfig.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace BFTools.Visuals.Background
+{
+    [CreateAssetMenu(fileName = "BFGradientLayerConfig", menuName = "BFTools/Visuals/Background/Gradient Layer Config")]
+    public class BFGradientLayerConfig : BFBackgroundLayerConfig
+    {
+        [SerializeField] private Color topColor = new Color(0.016f, 0.024f, 0.067f);
+        [SerializeField] private Color bottomColor = new Color(0.039f, 0.047f, 0.118f);
+
+        internal Color TopColor => topColor;
+        internal Color BottomColor => bottomColor;
+
+        public override IBFBackgroundLayer CreateLayer()
+        {
+            return new BFGradientLayer(this);
+        }
+    }
+}
