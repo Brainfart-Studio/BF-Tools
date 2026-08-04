7b08cdbb7ae5ed1badd02c1139fc047e471bc1cd
Mitchell Opitz <mitchell@brainfart.studio>
2026-08-03 22:33:09 -0500

    feat - Implement static SceneLoader with dictionary-tracked concurrent preloads

---
+++ Packages/Core/SceneManager/Runtime/BFSceneLoader.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using BFTools.Core.Logger;
+
+namespace BFTools.Core.SceneManager
+{
+    public static class BFSceneLoader
+    {
+        private const string LogTag = "SceneManager";
+
+        private static readonly Dictionary<string, AsyncOperation> operations = new Dictionary<string, AsyncOperation>();
+
+        public static Task LoadAsync(string sceneName, LoadSceneMode mode = LoadSceneMode.Additive)
+        {
+            if (operations.ContainsKey(sceneName))
+            {
+                BFLogger.Debug(LogTag, $"'{sceneName}' is already loading or loaded. Ignoring duplicate load request.");
+                return Task.CompletedTask;
+            }
+
+            AsyncOperation operation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName, mode);
+            operations[sceneName] = operation;
+            BFLogger.Debug(LogTag, $"Loading '{sceneName}' ({mode}).");
+
+            return AwaitCompletion(sceneName, operation);
+        }
+
+        public static void Preload(string sceneName, LoadSceneMode mode = LoadSceneMode.Additive)
+        {
+            if (operations.ContainsKey(sceneName))
+            {
+                BFLogger.Debug(LogTag, $"'{sceneName}' is already loading or loaded. Ignoring duplicate preload request.");
+                return;
+            }
+
+            AsyncOperation operation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName, mode);
+            operation.allowSceneActivation = false;
+            operations[sceneName] = operation;
+            BFLogger.Debug(LogTag, $"Preloading '{sceneName}' ({mode}).");
+        }
+
+        public static Task UnloadAsync(string sceneName)
+        {
+            operations.Remove(sceneName);
+            BFLogger.Debug(LogTag, $"Unloading '{sceneName}'.");
+
+            AsyncOperation operation = UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(sceneName);
+            return AwaitCompletion(operation);
+        }
+
+        private static Task AwaitCompletion(string sceneName, AsyncOperation operation)
+        {
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+            operation.completed += _ =>
+            {
+                operations.Remove(sceneName);
+                tcs.SetResult(true);
+            };
+            return tcs.Task;
+        }
+
+        private static Task AwaitCompletion(AsyncOperation operation)
+        {
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+            operation.completed += _ => tcs.SetResult(true);
+            return tcs.Task;
+        }
+    }
+}
