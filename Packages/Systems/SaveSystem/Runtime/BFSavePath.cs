c76bc4a1f43e8c1c4221dcf581f2160e994a01c1
Mitchell Opitz <mitchell@brainfart.studio>
2026-08-05 09:14:57 -0500

    fix - Isolate save key path in tests from the real persistentDataPath

--- Packages/Systems/SaveSystem/Runtime/BFSavePath.cs
+++ Packages/Systems/SaveSystem/Runtime/BFSavePath.cs
@@ -4,7 +4,9 @@ namespace BFTools.Systems.SaveSystem
 {
     public static class BFSavePath
     {
-        public static string DefaultDirectory => Application.persistentDataPath;
+        private static string directoryOverride;
+
+        public static string DefaultDirectory => directoryOverride ?? Application.persistentDataPath;
 
         public static string KeyFilePath => System.IO.Path.Combine(DefaultDirectory, "save.key");
     }
