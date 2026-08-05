22d8c9be4ee78e4bd2356f71ed0aeb1d630b37a1
Mitchell Opitz <mitchell@brainfart.studio>
2026-08-04 20:42:53 -0500

    feat - Add Save manager unit tests

---
+++ Packages/Systems/SaveSystem/Tests/Runtime/BFSaveManagerTests.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BFTools.Systems.SaveSystem;
+using Assert = NUnit.Framework.Assert;
+
+namespace BFTools.Systems.SaveSystem.Tests
+{
+    public class BFSaveManagerTests
+    {
+        private class FakeState
+        {
+            public int value;
+        }
+
+        private class FakeSaveable : ISaveable
+        {
+            public FakeState State = new FakeState();
+
+            public Type StateType => typeof(FakeState);
+            public object CaptureState() => State;
+            public void RestoreState(object state) => State = (FakeState)state;
+        }
+
+        private string scratchDir;
+
+        [SetUp]
+        public void SetUp()
+        {
+            ResetState();
+
+            scratchDir = Path.Combine(Path.GetTempPath(), "BFSaveManagerTests");
+            if (Directory.Exists(scratchDir))
+                Directory.Delete(scratchDir, true);
+            Directory.CreateDirectory(scratchDir);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ResetState();
+
+            if (Directory.Exists(scratchDir))
+                Directory.Delete(scratchDir, true);
+        }
+
+        private static void ResetState()
+        {
+            Type managerType = typeof(BFSaveManager);
+
+            List<ISaveable> saveables = (List<ISaveable>)managerType
+                .GetField("saveables", BindingFlags.NonPublic | BindingFlags.Static)
+                .GetValue(null);
+            saveables.Clear();
+
+            List<BFSaveSlot> slots = (List<BFSaveSlot>)managerType
+                .GetField("slots", BindingFlags.NonPublic | BindingFlags.Static)
+                .GetValue(null);
+            slots.Clear();
+
+            HashSet<Type> registeredStateTypes = (HashSet<Type>)managerType
+                .GetField("registeredStateTypes", BindingFlags.NonPublic | BindingFlags.Static)
+                .GetValue(null);
+            registeredStateTypes.Clear();
+        }
+
+        private static int GetSaveablesCount()
+        {
+            List<ISaveable> saveables = (List<ISaveable>)typeof(BFSaveManager)
+                .GetField("saveables", BindingFlags.NonPublic | BindingFlags.Static)
+                .GetValue(null);
+            return saveables.Count;
+        }
+
+        [Test]
+        public void Register_AddsSaveable_Unregister_RemovesSaveable()
+        {
+            FakeSaveable saveable = new FakeSaveable();
+
+            BFSaveManager.Register(saveable);
+            Assert.AreEqual(1, GetSaveablesCount());
+
+            BFSaveManager.Unregister(saveable);
+            Assert.AreEqual(0, GetSaveablesCount());
+        }
+
+        [Test]
+        public void Register_SameSaveableTwice_OnlyAddedOnce()
+        {
+            FakeSaveable saveable = new FakeSaveable();
+
+            BFSaveManager.Register(saveable);
+            BFSaveManager.Register(saveable);
+
+            Assert.AreEqual(1, GetSaveablesCount());
+        }
+
+        [Test]
+        public void RegisterSlot_NewSlotName_AddsSlot()
+        {
+            BFSaveManager.RegisterSlot(new BFSaveSlot { slotName = "Slot1", metadata = default });
+
+            bool found = BFSaveManager.TryGetSlot("Slot1", out BFSaveSlot slot);
+
+            Assert.IsTrue(found);
+            Assert.AreEqual("Slot1", slot.slotName);
+        }
+
+        [Test]
+        public void RegisterSlot_ExistingSlotName_UpdatesInPlace()
+        {
+            BFSaveManager.RegisterSlot(new BFSaveSlot { slotName = "Slot1", metadata = new BFSaveMetadata { version = 1 } });
+            BFSaveManager.RegisterSlot(new BFSaveSlot { slotName = "Slot1", metadata = new BFSaveMetadata { version = 2 } });
+
+            BFSaveManager.TryGetSlot("Slot1", out BFSaveSlot slot);
+
+            Assert.AreEqual(2, slot.metadata.version);
+        }
+
+        [Test]
+        public void TryGetSlot_UnknownSlotName_ReturnsFalse()
+        {
+            bool found = BFSaveManager.TryGetSlot("Missing", out _);
+
+            Assert.IsFalse(found);
+        }
+
+        [Test]
+        public void GetFileNameForSlot_ReturnsExpectedFormat()
+        {
+            string fileName = BFSaveManager.GetFileNameForSlot("Slot1");
+
+            Assert.AreEqual("save_Slot1.dat", fileName);
+        }
+
+        [Test]
+        public async Task SaveAsync_LoadAsync_RoundTrip_PersistsAndRestoresRegisteredSaveableState()
+        {
+            FakeSaveable saveable = new FakeSaveable { State = new FakeState { value = 42 } };
+            BFSaveManager.Register(saveable);
+
+            await BFSaveManager.SaveAsync("Slot1", scratchDir);
+            saveable.State = new FakeState { value = 0 };
+
+            bool loaded = await BFSaveManager.LoadAsync("Slot1", scratchDir);
+
+            Assert.IsTrue(loaded);
+            Assert.AreEqual(42, saveable.State.value);
+            Assert.IsTrue(BFSaveManager.TryGetSlot("Slot1", out BFSaveSlot slot));
+            Assert.AreEqual(BFSaveVersionMigrator.CurrentVersion, slot.metadata.version);
+        }
+
+        [Test]
+        public async Task LoadAsync_NoSaveFileExists_ReturnsFalse()
+        {
+            bool loaded = await BFSaveManager.LoadAsync("MissingSlot", scratchDir);
+
+            Assert.IsFalse(loaded);
+        }
+    }
+}
