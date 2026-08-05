84686f9ed04d728ede77922be460f9d33cbf39fb
Mitchell Opitz <mitchell@brainfart.studio>
2026-08-04 23:08:03 -0500

    fix - Let GlobalBootstrapper PlayMode test actually enter play mode instead of restricting to editor platform

--- Packages/Systems/GlobalBootstrapper/Tests/PlayMode/BFGlobalBootstrapperPlayModeTests.cs
+++ Packages/Systems/GlobalBootstrapper/Tests/PlayMode/BFGlobalBootstrapperPlayModeTests.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections.Generic;
 using System.Reflection;
 using NUnit.Framework;
@@ -120,3 +121,4 @@ namespace BFTools.Systems.GlobalBootstrapper.PlayModeTests
         }
     }
 }
+#endif
