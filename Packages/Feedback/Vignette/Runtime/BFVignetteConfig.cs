14f0d0a65459b7298e491a7357849d2af8b3e17c
Mitchell Opitz <mitchell@brainfart.studio>
2026-08-13 22:08:24 -0500

    feat - Scaffold Vignette config in Feedback package

---
+++ Packages/Feedback/Vignette/Runtime/BFVignetteConfig.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BFTools.Feedback.Vignette
+{
+    public enum BFVignetteBlendMode
+    {
+        AlphaBlend,
+        Multiply
+    }
+
+    [Serializable]
+    public struct BFVignetteEntry
+    {
+        public string eventName;
+        [Range(0f, 1f)] public float intensity;
+        public float radius;
+        public float softness;
+        public Color color;
+        [Range(0f, 1f)] public float roundness;
+        public float duration;
+        public AnimationCurve intensityCurve;
+        public BFVignetteBlendMode blendMode;
+    }
+
+    public class BFVignetteConfig : ScriptableObject
+    {
+        [SerializeField] private List<BFVignetteEntry> entries = new List<BFVignetteEntry>();
+
+        public IReadOnlyList<BFVignetteEntry> Entries => entries;
+
+        public bool TryGetEntry(string eventName, out BFVignetteEntry entry)
+        {
+            for (int i = 0; i < entries.Count; i++)
+            {
+                if (entries[i].eventName == eventName)
+                {
+                    entry = entries[i];
+                    return true;
+                }
+            }
+
+            entry = default;
+            return false;
+        }
+    }
+}
